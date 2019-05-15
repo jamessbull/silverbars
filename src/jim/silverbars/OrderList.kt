62ff48ecac0af527db88147cb0ac317791f956c5
jamesbull <james.s.bull@gmail.com>
2019-05-15 23:46:41 +0100

    Remove unused code

--- src/jim/silverbars/OrderList.kt
+++
@@ -1,9 +0,0 @@
-package jim.silverbars
-
-fun List<Order>.toOrderSummaries(): List<OrderSummary> {
-    return this
-        .groupingBy { o -> o.price }
-        .fold(OrderSummary.empty()) { summary, order -> summary + order }
-        .values
-        .sortedByDescending { it.price }
-}
